546e07b453ebb831e92f94dfdda397db9104c724
Charles Macharia <charlesmk2018@gmail.com>
2022-02-11 03:14:00 +0300

    Changes
    Introduced the GIS module(open street)

--- DonationsPlatform/settings.py
+++ DonationsPlatform/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'maps',
+    'django.contrib.gis'
 ]
 
 MIDDLEWARE = [
@@ -75,7 +78,7 @@ WSGI_APPLICATION = 'DonationsPlatform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -116,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = ''
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- maps/admin.py
+++ maps/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Marker
+
 
 # Register your models here.
+
+@admin.register(Marker)
+class MarkerAdmin(admin.GISModelAdmin):
+    list_display = ("name", "location")
