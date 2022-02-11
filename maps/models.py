008b66ff6c7c632d4c5283223ec80ef12ff84828
Charles Macharia <charlesmk2018@gmail.com>
2022-02-11 03:17:17 +0300

    Model and View change
    Introduced a marker model

--- maps/models.py
+++ maps/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.gis.db.models import PointField
 # Create your models here.
+
+
+class Marker(models.Model):
+    name = models.CharField(max_length=255)
+    location = PointField()
